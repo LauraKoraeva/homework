1ef04eb5f4cf7900be0760793b6f34b93e0111ca
Laura <laurakoraeva@gmail.com>
2024-10-11 17:40:51 +0300

    edited

--- module_1_lesson_11.cpp
+++ module_1_lesson_11.cpp
@@ -59,6 +59,127 @@ int main()
 
 
 
+//Задание 3. Валидация IP-адреса
+
+#include <iostream>
+#include <string>
+
+bool check_length(std::string ip)
+{
+    int points = 0;
+    int symbols = 0;
+    for (int i = 0; i < ip.size(); ++i)
+    {
+        if (ip[i] == '.')
+            ++points;
+        else
+            ++symbols;
+    }
+
+    if ((points > 3 || points < 3) || (symbols + points > 15 || symbols + points < 7))
+        return false;
+    else
+        return true;
+}
+
+bool check_symbols(std::string ip)
+{
+    for (int i = 0; i < ip.size(); ++i)
+    {
+        if ((ip[i] < 0 || ip[i] > '9') && ip[i] != '.')
+            return false;
+    }
+    return true;
+}
+
+bool check_points(std::string ip)
+{
+    if (ip[0] == '.' || ip[ip.size() - 1] == '.')
+        return false;
+   
+    for (int i = 0; i < ip.size() - 1; ++i)
+    {
+        if (ip[i] == '.' && ip[i + 1] == '.')
+            return false;
+    }
+    return true;
+}
+
+bool check_zeros(std::string ip)
+{
+    for (int i = 0; i < ip.size() - 2; ++i)
+    {
+        if (ip[i] == '.' && ip[i + 1] == '0' && ip[i + 2] != '.')
+            return false;
+    }
+    return true;
+}
+
+int get_address_part(std::string ip, int count)
+{
+    std::string octa;
+    int octaNumber;
+    int pointIndex;
+    int countPoint = 0;
+
+    if (count == 0)
+        octa = ip.substr(0, ip.find('.'));
+    else
+    {
+        for (int i = 0; i < ip.size(); ++i)
+        {
+            if (ip[i] == '.')
+            {
+                pointIndex = i;
+                ++countPoint;
+                if (countPoint == count)
+                    octa = ip.substr(pointIndex + 1, ip.find('.'));
+            }
+        }
+    }
+    octaNumber = stoi(octa);
+
+    return octaNumber;
+}
+
+bool check_range(std::string ip)
+{
+    for (int count = 0; count < 4; ++count)
+    {
+        int octaNumber = get_address_part(ip, count);
+        if (octaNumber < 0 || octaNumber > 255)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    std::cout << "Enter IP address: ";
+    std::string ip;
+    std::cin >> ip;
+
+    if (check_length(ip) && check_symbols(ip) && check_points(ip) && check_zeros(ip))
+    {
+        if (!check_range(ip))
+            std::cout << "Invalid\n";
+        else
+            std::cout << "Valid\n";
+    }
+    else
+        std::cout << "Invalid\n";
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
 //Задание 4. Оценка результатов игры в крестики-нолики
 
 #include <iostream>
--- module_1_lesson_8.cpp
+++ module_1_lesson_8.cpp
@@ -115,23 +115,28 @@ int main()
 
 int main()
 {
-    float kts = 4400.0f; 
+    float kts = 4400.0f;
     float gravitsapa = kts / 2;
 
-    std::cout << "Введите количество чатлов: ";
-    int chatlNumber;
+    std::cout << "Enter the amount of chatls: ";
+    float chatlNumber;
     std::cin >> chatlNumber;
-    
-    float ktsNumber = (float) chatlNumber / kts;
-    
-    int gravitsapaCount = (float) chatlNumber / gravitsapa;
    
-    std::cout << chatlNumber << " ч = " << ktsNumber << " КЦ\n";
-    std::cout << "Количество гравицап, которое можно купить: " << gravitsapaCount;
+    while ((int) chatlNumber - chatlNumber != 0 || chatlNumber < 0)
+    {
+        std::cout << "Incorrect input. Try again!\n";
+        std::cin >> chatlNumber;
+    }
 
+    float ktsNumber = chatlNumber / kts;
 
+    int gravitsapaCount = chatlNumber / gravitsapa;
 
-    return 0;
+    std::cout << chatlNumber << " ch = " << ktsNumber << " KTS\n";
+    std::cout << "The number of gravitsapas you can buy: " << gravitsapaCount;
+
+return 0;
+
 }
 
 
@@ -149,24 +154,22 @@ int main()
 
 int main()
 {
-    float x, y;
+    double x, y;
     bool correctInput = false;
     do
     {
-        std::cout << "Введите местоположение фигуры: \n";
+        std::cout << "Enter the coordinates of the chess piece: \n";
         std::cin >> x >> y;
-
         if (x > 0.799 || x < 0 || y > 0.799 || y < 0)
-            std::cout << "Местоположение неверное.";
-        else
-            correctInput = true;
+            std::cout << "Incorrect input. Try again!";
+        else correctInput = true;
     } while (!correctInput);
 
     x *= 10;
     y *= 10;
 
-    std::cout << "Фигура находится в клетке (" << (int) x << ", " << (int) y << ").\n";
-    
+    std::cout << "Chess piece is on square (" << (int) x << ", " << (int) y << ").\n";
+
     return 0;
 }
 
@@ -217,6 +220,45 @@ int main()
     return 0;
 }
 
+
+
+
+
+
+
+
+
+
+//Задача 4. Аккуратность
+
+#include <iostream>
+
+int main()
+{
+    double x, y;
+    bool correctInput = false;
+    do
+    {
+        std::cout << "Enter the coordinates of the chess piece: \n";
+        std::cin >> x >> y;
+        if (x > 0.799 || x < 0 || y > 0.799 || y < 0)
+            std::cout << "Incorrect input. Try again!";
+        else correctInput = true;
+    } while (!correctInput);
+
+    int position_x = x * 10;
+    int position_y = y * 10;
+
+    double adjust_x, adjust_y;
+   
+    adjust_x = 0.050 - (x - (double) position_x / 10);
+    adjust_y = 0.050 - (y - (double) position_y / 10);
+    
+   std::cout << "Chess piece is on square (" << position_x << ", " << position_y << ").\n"; 
+   std::cout << "Adjust the position of the piece by (" << adjust_x << ", " << adjust_y << ").\n";
+
+    return 0;
+}
 
 
 
